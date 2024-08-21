6b06ad0282d9062dea3ff772463c27e011735fab
woolw <woolw@pm.me>
2024-08-22 01:06:29 +0200

    fixed missing zoxide

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install alacritty arc iina steam syncplay font-jetbrains-mono-nerd-font odin stow git neovim firefox
+brew install alacritty arc iina steam syncplay font-jetbrains-mono-nerd-font odin stow git neovim firefox zoxide
 
 stow alacritty &
 stow zsh &
