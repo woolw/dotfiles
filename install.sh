34c7b8ab05492835d7efbe40421ba17b799ba781
woolw <woolw@pm.me>
2024-08-22 00:00:35 +0200

    improved install script and alacrity font

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install alacritty arc iina steam syncplay font-jetbrains-mono-nerd-font odin stow git neovim firefox
+
 stow alacritty &
 stow zsh &
